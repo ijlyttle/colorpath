aae9d4e79ebe22331b93df222e6ce1dcbe45fac7
Ian Lyttle <ijlyttle@users.noreply.github.com>
2020-09-24 18:00:23 -0500

    bump version, add rescale_palette(), tests (#13)

---
+++ R/rescale-palette.R
@@ -0,0 +1,36 @@
+#' Rescale palette
+#'
+#' Rescale a palette function using a rescaler function.
+#'
+#' @param palette `function` with S3 class `"cpath_palette_luv"`
+#' @param rescaler `function` with S3 class `"cpath_rescaler"`
+#'
+#' @return `function` with S3 class `"cpath_palette_luv"`
+#' @examples
+#'   # create original palette
+#'   pal_blues <- palette_bezier(mat_luv_blues)
+#'
+#'   # create rescaler
+#'   rsc_reverse <- rescaler_linear_input(c(1, 0))
+#'
+#'   # create reversed palette
+#'   pal_blues_reverse <- rescale_palette(pal_blues, rsc_reverse)
+#'
+#'   pal_blues(0)
+#'   pal_blues_reverse(1)
+#'
+#' @export
+#'
+rescale_palette <- function(palette, rescaler) {
+
+  assertthat::assert_that(
+    inherits(palette, "cpath_palette_luv"),
+    inherits(rescaler, "cpath_rescaler")
+  )
+
+  .f <- function(x) {
+    palette(rescaler(x))
+  }
+
+  structure(.f, class = "cpath_palette_luv")
+}
