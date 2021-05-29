88d9b379bea9ff539849ae0315ce0b2b095e76d0
Ian Lyttle <ian.lyttle@schneider-electric.com>
2021-05-29 12:33:36 -0500

    add `[` method for pth_mat

--- R/creator.R
+++ R/creator.R
@@ -18,3 +18,23 @@ pth_creator.default <- function(mat, ...) {
     glue::glue("No method for class {class(mat)}")
   )
 }
+
+#' @export
+#'
+`[.pth_mat` <- function(x, i, ...) {
+
+  # we need this so that when we subset, the rest of the
+  # attributes "come along for the ride"
+
+  # subset normally, don't drop dimensions
+  mat <- NextMethod(drop = FALSE)
+
+  # if we don't have three columns, no classes, no attributes
+  if (!identical(ncol(mat), 3L)) {
+    return(mat)
+  }
+
+  creator <- pth_creator(x)
+
+  creator(mat)
+}
